8f0326e1dedabfe7e3bc133247c269896fcd8047
AIT-AS <a.schuetz79@gmail.com>
2025-11-25 21:53:08 +0100

    added Homework04

---
+++ src/homeworks/Homework_04/Homework_04.jsx
@@ -0,0 +1,12 @@
+import "./styles.css";
+import Feedback from "../../components/Feedback/Feedback";
+
+function Homework_04() {
+    return (
+        <div className="homework_04_wrapper">
+            <Feedback />
+        </div>
+    );
+}
+
+export default Homework_04;
