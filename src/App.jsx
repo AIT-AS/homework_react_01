4859f1d268eead578a1da07f1039026e121d6d8f
AIT-AS <a.schuetz79@gmail.com>
2025-11-26 21:02:29 +0100

    added Counter, App.jsx, styles

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import Lesson_05 from "./lessons/Lesson_05/Lesson_05";
 // Homeworks
 import Homework_02 from "./homeworks/Homework_02/Homework _02";
 import Homework_03 from "./homeworks/Homework_03/Homework_03";
+import Homework_04 from "./homeworks/Homework_04/Homework_04";
+
 
 // Consultation
 
@@ -28,7 +30,8 @@ function App() {    // Объявление компонента App как фу
 
       {/* Homeworks */}
       {/* <Homework_02 /> */}
-      <Homework_03 />
+      {/* <Homework_03 /> */}
+      <Homework_04 />
 
       {/* Consultation */}
       {/* <Consultation_03 /> */}
